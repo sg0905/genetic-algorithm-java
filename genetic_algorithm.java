bf40b6bf6da86cde3902ff991df689fed052a6c1
Shubham Kumar Gupta <shubhamthe9@gmail.com>
2024-05-08 23:26:40 +0530

    Genetic algorithm in java

--- genetic_algorithm.java
+++ genetic_algorithm.java
@@ -1,7 +1,42 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;import java.util.Random;
 class genetic_algorithm{
-    public static void main(String args[])
-    {
-        System.out.println("Shubham");
+    
+    private static final int pop_size =50;
+    private static final int gene_length = 5;
+    private static final double max_val= 5.12;
+    private static final double mutation =0.1;
+    private static final int max_gen=1000;
+
+    private static Random random = new Random();
+    private static class Chromosome {
+        double gene[];
+        double fitness;
+
+        Chromosome(){
+
         }
+    }
+
+
+    private static double objectiveFunction(double []x)
+    {
+        double sum=0;
+        for(double value :x)
+        {
+            sum=sum + Math.pow(value,2);
+        }
+        return sum;
+    }
+    
+    
+    
+    
+    
+    public static void main(String args[]){
+
+    }
 
 }
