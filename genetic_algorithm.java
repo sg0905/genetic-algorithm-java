d2ea9d03b8f43a11bea5fd451a2094fb0d298fb1
Shubham Kumar Gupta <shubhamthe9@gmail.com>
2024-05-08 23:42:24 +0530

    Genetic Algorithm

--- genetic_algorithm.java
+++ genetic_algorithm.java
@@ -5,10 +5,10 @@ import java.util.List;import java.util.Random;
 class genetic_algorithm{
     
     private static final int pop_size =50;
-    private static final int gene_length = 5;
-    private static final double max_val= 5.12;
+    private static final int chromosome_length = 10;
     private static final double mutation =0.1;
     private static final int max_gen=1000;
+   
 
     private static Random random = new Random();
     private static class Chromosome {
@@ -36,7 +36,35 @@ class genetic_algorithm{
     
     
     public static void main(String args[]){
+        int population [][]= initializePopulation();
+        int generation = 0;
 
+        while (generation < max_gen)
+        {
+            int [][] newPopulation= new int [pop_size][chromosome_length];
+            for(int i =0; i< pop_size; i++){
+                int parent1[]= tournamentSelection(population);
+                int parent2 []= tournamentSelection(population);
+                int offspring[]= crossover(parent1, parent2);
+                mutate (offspring);
+                newPopulation =offspring;
+            }
+            population=newPopulation;
+            generation++;
+            System.out.println("Generation "+ generation+ " Fitness: "+ getFitness(population));
+        }
     }
+    private static int [][] initializePopulation(){
+        int [][] population=new int [pop_size][chromosome_length];
+        Random rand =new Random();
+        for(int i=0;i<pop_size;i++){
+            for(int j=0;j<chromosome_length;j++)
+            {
+                population[i][j]= rand.nextInt(2);
+            }
+        }
+        return population;
+    }
+    
 
 }
